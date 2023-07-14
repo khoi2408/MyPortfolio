46faa488de65461c42b027cbda498e6bd52c578b
khoi le <lmkhoi2k@gmail.com>
2023-07-14 13:16:47 +0700

    first commit

---
+++ components/aboutPage/About.tsx
@@ -0,0 +1,23 @@
+import AboutMe from "./AboutMe"
+import BrandLogos from "./BrandLogos"
+import Facts from "./Facts"
+import MyServices from "./MyServices"
+import Prices from "./Prices"
+import Testimonials from "./Testimonials"
+import Title from "../Title"
+
+export default function About() {
+  return (
+    <section className="h-full overflow-y-scroll myScroll">
+      <Title name="about me" />
+      <AboutMe />
+
+      <Title name="my skills" />
+      <MyServices />
+
+      <Title name="clients" />
+      <BrandLogos />
+
+    </section>
+  )
+}
