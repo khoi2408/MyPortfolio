c8c0d41a289bd7229f32289c59cef142578b027d
khoi le <lmkhoi2k@gmail.com>
2023-07-17 13:45:29 +0700

    fix deploy

--- graphqlOperations/contact.ts
+++ graphqlOperations/contact.ts
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   Mutation: {
     sendMessage: gql`
--- graphqlOperations/resume.ts
+++ graphqlOperations/resume.ts
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   Queries: {
     getExperience: gql`
